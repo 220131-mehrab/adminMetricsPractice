04d9b3a50cf27d8911713e4486759b6dd3e02c17
ChristysGitHub <chrndaye@gmail.com>
2022-04-18 00:45:04 -0700

    Manipulate health of Actualator

---
+++ backend/src/main/java/com/revature/employeemanager/monitoring/CustomActuatorEndpoint.java
@@ -0,0 +1,19 @@
+package com.revature.employeemanager.monitoring;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Endpoint(id = "custom")
+@Component
+public class CustomActuatorEndpoint {
+    @ReadOperation
+    public Map<String, String> customEndpoint() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Key", "Value");
+        return map;
+    }
+}
--- backend/src/main/java/com/revature/employeemanager/monitoring/DatabaseService.java
+++ backend/src/main/java/com/revature/employeemanager/monitoring/DatabaseService.java
@@ -1,5 +1,24 @@
 package com.revature.employeemanager.monitoring;
 
-public class DatabaseService {
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
 
+@Component
+public class DatabaseService implements HealthIndicator {
+
+    private final String DATABASE_SERVICE = "Database Service";
+
+    @Override
+    public Health health() {
+        if(isDatabaseHealthGood()){
+            return Health.up().withDetail(DATABASE_SERVICE, "Service is running").build();
+        }
+        return Health.down().withDetail(DATABASE_SERVICE, "Service is running").build();
+    }
+
+    private boolean isDatabaseHealthGood(){
+        //logic
+        return true;
+    }
 }
